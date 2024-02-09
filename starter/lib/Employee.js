9dbf9a27c00aad371346953cf0517b1f9cc68c35
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-02-09 21:09:09 +0000

    added parent class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(id, name, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  //methods
+
+  getId() {}
+  getName() {}
+  getEmail() {}
+  getRole() {
+    return "Employee";
+  }
+}
+
+module.exports = Employee;
--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+  constructor(id, name, email, github) {
+    // super is calling the superclass constructor with optional arguments
+    super(id, name, email);
+    this.github = github;
+  }
+
+  getGithub() {}
+
+  getRole() {
+    super.getRole();
+    return Engineer;
+  }
+}
+
+const rectangle = new Rectangle(12, 9);
+rectangle.printInfo();
